f505df59c16a59de36019f00b3c3cf227e20e322
Paul Gortmaker <paul.gortmaker@windriver.com>
2009-01-25 17:58:09 -0500

    modpost: mask trivial warnings
    Newer HOSTCC will complain about various stdio fcns because
    they are now declared with "warn_unused_return", or because
    some initializers aren't filled out with values -- trivially
    assign values so we aren't overwhelmed with these warnings
    once the kernel build kicks off.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1568,7 +1568,7 @@ static void read_symbols(const char *modname)
 	char *license;
 	char *namespace;
 	struct module *mod;
-	struct elf_info info = { };
+	struct elf_info info = { .hdr = NULL };
 	Elf_Sym *sym;
 
 	if (!parse_elf(&info, modname))
@@ -2195,7 +2195,7 @@ fail:
 
 static void write_dump(const char *fname)
 {
-	struct buffer buf = { };
+	struct buffer buf = { NULL, 0, 0 };
 	struct module *mod;
 	struct symbol *sym;
 
