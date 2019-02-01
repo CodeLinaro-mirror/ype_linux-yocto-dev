cb13ceb516c4488bdd9c3d655b0af4c4026c03bd
Stanislaw Kardach <skardach@marvell.com>
2019-02-01 14:19:51 +0530

    octeontx2-af: Add resource partitioning config
    commit 03edea00f9751eeabc12eed3012d06ea1a153e0f from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This commit adds facilities for configuring and tracking resource
    partitioning between RVU PF devices (and indirectly their related
    RVU VFs).
    The AF keeps track of the PF level resource partitioning only because
    partitioning between VFs is a job of each RVU PF and only RVU PF driver
    knows the moment and number of VFs enabled.
    
    For each tracked resource (currently RVU LFs), user can set a limit
    for each RVU PF on how many resources may be attached to it via
    MBOX (MBOX_MSG_ATTACH_RESOURCES msg for LFs).
    This limit value K for PF X means that the sum of attach requests
    for that resource coming from all pcifunc belonging to PF x (called
    PF x's family) cannot exceed K.
    The sum of limit values for a particular resource always has to be lower
    or equal to the total amount of those resources on the system.
    
    The limits are modifiable for PFx until resource attach message is
    received. At that point they become read-only until all resources are
    detached from that device.
    
    The default values for the limits are defined as follows:
    * RVU AF (for its future VF handling)
      - NIX: pci_sriov_get_totalvfs(pdev)
      - NPA: pci_sriov_get_totalvfs(pdev)
    * networking RVU PF
      - NIX: 1 + pci_sriov_get_totalvfs(pdev)
      - NPA: 1 + pci_sriov_get_totalvfs(pdev)
      - rest: 0
    * SSO/TIM RVU PF
      - NPA: pci_sriov_get_totalvfs(pdev)
      - SSO: N(SSO_LFS) / N(SSO_PFS)
      - SSOW: N(SSOW_LFS) / N(SSO_PFS)
      - TIM: N(TIM_LFS) / N(SSO_PFS)
      - CPT: num_online_cpus() / N(SSO_PFS)
      - rest: 0
    * NPA RVU PF
      - NPA: pci_sriov_get_totalvfs(pdev)
      - rest: 0
    * CPT RVU PF
      - CPT: num_online_cpus()
      - NPA: 1
      - rest: 0
    
    Change-Id: I602ddc19f87c8d863e92eb05c4d933dd39af3d48
    Signed-off-by: Stanislaw Kardach <skardach@marvell.com>
    Signed-off-by: Krzysztof Garczynski <kgarczynski@marvell.com>
    [Kevin: Just some minor context mods in order to port to linux-yocto]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -1377,6 +1377,12 @@ static int rvu_check_rsrc_availability(struct rvu *rvu,
 	struct rvu_hwinfo *hw = rvu->hw;
 	struct rvu_block *block;
 
+	if (rvu_check_rsrc_policy(rvu, req, pcifunc)) {
+		dev_err(rvu->dev, "Func 0x%x: Resource policy check failed\n",
+			pcifunc);
+		return -EINVAL;
+	}
+
 	/* Only one NPA LF can be attached */
 	if (req->npalf && !is_blktype_attached(pfvf, BLKTYPE_NPA)) {
 		block = &hw->block[BLKADDR_NPA];
@@ -2840,7 +2846,14 @@ static int rvu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	/* Initialize debugfs */
 	rvu_dbg_init(rvu);
 
+	err = rvu_policy_init(rvu);
+	if (err)
+		goto err_sriov;
+
 	return 0;
+
+err_sriov:
+	rvu_disable_sriov(rvu);
 err_dl:
 	rvu_unregister_dl(rvu);
 err_irq:
@@ -2873,6 +2886,7 @@ static void rvu_remove(struct pci_dev *pdev)
 	struct rvu *rvu = pci_get_drvdata(pdev);
 
 	rvu_dbg_exit(rvu);
+	rvu_policy_destroy(rvu);
 	rvu_unregister_interrupts(rvu);
 	rvu_unregister_dl(rvu);
 	rvu_flr_wq_destroy(rvu);
