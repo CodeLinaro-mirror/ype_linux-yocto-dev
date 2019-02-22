c3454775689c24e1598a5aea1a463d6816975bf7
Grzegorz Jaszczyk <jaz@semihalf.com>
2019-02-22 13:53:21 +0100

    net: phy: add support for 5GBASE-KR PHY connection types
    commit 29e0da9a92e2ab04e9c30b2a18c6048634aecd4b from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The 5GBASE-KR is a single lane Serdes backplane ehtenet. Some PHYs use
    it to connect 5G ethernet different than 5GBASE-KR, e.g. 88E2110
    meeting 5gbase-t requires single serdes lane to be configured to 5G.
    
    Change-Id: I274c879912ec2732d57d29b3c2e3648c1b7fb66d
    Signed-off-by: Grzegorz Jaszczyk <jaz@semihalf.com>
    Reviewed-on: https://sj1git1.cavium.com/4403
    Reviewed-by: Stefan Chulski <Stefan.Chulski@cavium.com>
    Reviewed-by: Marcin Wojtas <Marcin.Wojtas@cavium.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/37773
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Stefan Chulski <stefanc@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -148,6 +148,8 @@ typedef enum {
 	PHY_INTERFACE_MODE_USXGMII,
 	/* 10GBASE-KR - with Clause 73 AN */
 	PHY_INTERFACE_MODE_10GKR,
+	/* 5GBASE-KR - Single lane 5G Serdes */
+	PHY_INTERFACE_MODE_5GKR,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -223,6 +225,8 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "usxgmii";
 	case PHY_INTERFACE_MODE_10GKR:
 		return "10gbase-kr";
+	case PHY_INTERFACE_MODE_5GKR:
+		return "5gbase-kr";
 	case PHY_INTERFACE_MODE_100BASEX:
 		return "100base-x";
 	default:
