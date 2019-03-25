6339b28651a42115572410bdb3aa0e4b51e93f8c
Nithin Dabilpuram <ndabilpuram@marvell.com>
2019-03-25 14:32:16 +0530

    octeontx2-af: add support for changing vlan tpid
    commit 4763752d8fbeeaf5c00deaa4014a0efb71ad5e5c from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    During nix_lf_alloc(), TPIDs of VLAN0/1 are configured with
    '0x88A8' and '0x8100'. This change adds support to
    configure/change them to custom ones.
    
    Change-Id: Ie8c58c1bc3692eeaddf18577723c80b34eb0b1a4
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>
    [Kevin: Just some minor context mods in order to port to linux-yocto]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -686,6 +686,9 @@ int rvu_get_nixlf_count(struct rvu *rvu);
 void rvu_nix_lf_teardown(struct rvu *rvu, u16 pcifunc, int blkaddr, int npalf);
 int nix_get_nixlf(struct rvu *rvu, u16 pcifunc, int *nixlf, int *nix_blkaddr);
 int nix_update_bcast_mce_list(struct rvu *rvu, u16 pcifunc, bool add);
+int rvu_mbox_handler_nix_set_vlan_tpid(struct rvu *rvu,
+				       struct nix_set_vlan_tpid *req,
+				       struct msg_rsp *rsp);
 struct nix_hw *get_nix_hw(struct rvu_hwinfo *hw, int blkaddr);
 int rvu_get_next_nix_blkaddr(struct rvu *rvu, int blkaddr);
 void rvu_nix_reset_mac(struct rvu_pfvf *pfvf, int pcifunc);
