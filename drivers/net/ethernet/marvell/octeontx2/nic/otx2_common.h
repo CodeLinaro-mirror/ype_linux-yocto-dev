68ac563811777793ddde183bf8f717bcc668008d
Tomasz Duszynski <tduszynski@marvell.com>
2019-04-12 16:51:29 +0530

    octeontx2-vf: Add ethtool support
    commit 62d1860ebf82042f86c812558bf8d35fde41d41b from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Add ethtool support.
    
    Change-Id: Ib18f2c2fc121ee87dae53113dd8245211e14cade
    Signed-off-by: Tomasz Duszynski <tduszynski@marvell.com>
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    [RQ: Drop the parts of which have been already applied.
     Use mutex_lock/unlock instead of otx2_mbox_lock/otx2_mbox_unlock.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -825,6 +825,7 @@ int otx2_add_macfilter(struct net_device *netdev, const u8 *mac);
 int otx2_enable_rxvlan(struct otx2_nic *pf, bool enable);
 int otx2_install_rxvlan_offload_flow(struct otx2_nic *pfvf);
 int otx2_destroy_ethtool_flows(struct otx2_nic *pfvf);
+int otx2_delete_vf_ethtool_flows(struct otx2_nic *pfvf);
 u16 otx2_get_max_mtu(struct otx2_nic *pfvf);
 /* tc support */
 int otx2_init_tc(struct otx2_nic *nic);
