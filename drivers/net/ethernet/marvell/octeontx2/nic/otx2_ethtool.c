196f04c454b062e7e339dad14b6d31cd65d47a43
Tomasz Duszynski <tduszynski@marvell.com>
2019-04-12 16:51:29 +0530

    octeontx2-vf: Add ethtool support
    commit 3aa2b83e6b64a19fc0073901a67e095609f1183f from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Add ethtool support.
    
    Change-Id: Ib18f2c2fc121ee87dae53113dd8245211e14cade
    Signed-off-by: Tomasz Duszynski <tduszynski@marvell.com>
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>
    [RQ: Drop the parts which have been already applied, replace inline
    function otx2_mbox_lock/otx2_mbox_unlock with mutex_lock/mutex_unlock.]
    Signed-off-by: Ruiqiang Hao <ruiqiang.hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
@@ -21,6 +21,7 @@
 
 #define DRV_NAME	"octeontx2-nicpf"
 #define DRV_VF_NAME	"octeontx2-nicvf"
+#define DRV_VF_VERSION	"1.0"
 
 #define OTX2_DEFAULT_ACTION	0x1
 
@@ -1678,6 +1679,40 @@ int otx2_destroy_ethtool_flows(struct otx2_nic *pfvf)
 }
 
 /* VF's ethtool APIs */
+int otx2_delete_vf_ethtool_flows(struct otx2_nic *pfvf)
+{
+	struct npc_delete_flow_req *req;
+	struct otx2_flow *iter, *tmp;
+	int err;
+
+	mutex_lock(&pfvf->mbox.lock);
+	req = otx2_mbox_alloc_msg_npc_delete_flow(&pfvf->mbox);
+	if (!req) {
+		mutex_unlock(&pfvf->mbox.lock);
+		return -ENOMEM;
+	}
+
+	req->all_vfs = 1;
+	/* Send message to AF */
+	err = otx2_sync_mbox_msg(&pfvf->mbox);
+	if (err) {
+		mutex_unlock(&pfvf->mbox.lock);
+		return err;
+	}
+
+	mutex_unlock(&pfvf->mbox.lock);
+	/* AF deleted VF entries now remove from ethtool list */
+	list_for_each_entry_safe(iter, tmp, &pfvf->flows, list) {
+		if (iter->is_vf) {
+			list_del(&iter->list);
+			kfree(iter);
+			pfvf->nr_flows--;
+		}
+	}
+
+	return 0;
+}
+
 static void otx2vf_get_drvinfo(struct net_device *netdev,
 			       struct ethtool_drvinfo *info)
 {
