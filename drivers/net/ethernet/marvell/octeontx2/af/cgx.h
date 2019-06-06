c2e0652f9143733257acfc177bccf3dd7a5b31b1
Vamsi Attunuru <vattunuru@marvell.com>
2019-06-06 12:30:12 +0530

    octeontx2-af: Handle physical link state change requests
    commit 60998c21e4a79eca61bd80c134c83dfd780ee545 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Patch adds support to toggle the cgx lmac link state
    through a mailbox message, mbox handler only allows
    cgx mapped rvu pfs to change the link state.
    
    Change-Id: I060a9a557f1b7a155d0c5b432f3dcf175bfad7ae
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.h
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.h
@@ -175,4 +175,5 @@ u8 cgx_get_lmacid(void *cgxd, u8 lmac_index);
 unsigned long cgx_get_lmac_bmap(void *cgxd);
 void cgx_lmac_write(int cgx_id, int lmac_id, u64 offset, u64 val);
 u64 cgx_lmac_read(int cgx_id, int lmac_id, u64 offset);
+int cgx_set_link_state(void *cgxd, int lmac_id, bool enable);
 #endif /* CGX_H */
