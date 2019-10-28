b3c0d48f2695034c0b3468b2fbc62afb28b2140e
hariprasad <hkelam@marvell.com>
2019-10-28 14:39:09 +0530

    octeontx2-pf: Support to enable EDSA/Higig2 pkts parsing
    commit 99fcbdddaa10828efdb4ad57a6042ed6b0a371ea from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    When switch headers like EDSA, Higig2 etc are present in ingress
    or egress pkts the pkt parsing done by NPC needs to take additional
    headers into account. KPU profile handles these using specific PKINDs
    (the iKPU index) to start parsing pkts differently.
    
    This patch enables user to configure these PKINDs into HW for proper
    pkt parsing. Patch also handles changes to max frame size due to
    additional headers in pkt.
    
    higig2:
            ethtool --set-priv-flags eth0 higig2 on/off
    edsa:
            ethtool --set-priv-flags eth0 edsa on/off
    
    Change-Id: If27c198a555b480434a5d56e15f0f7cff3800144
    Signed-off-by: hariprasad <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/17912
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    [RQ: Drop the parts which have been already applied.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -306,6 +306,7 @@ struct otx2_nic {
 	struct net_device	*netdev;
 	struct dev_hw_ops	*hw_ops;
 	void			*iommu_domain;
+	u16			xtra_hdr;
 	u16			max_frs;
 	u16			rbsize; /* Receive buffer size */
 
@@ -350,6 +351,7 @@ struct otx2_nic {
 #define OTX2_NTUPLE_FILTER_CAPABLE		0
 #define OTX2_UNICAST_FILTER_CAPABLE		1
 	unsigned long           priv_flags;
+
 	u16			entry_list[NPC_MAX_NONCONTIG_ENTRIES];
 	struct list_head	flows;
 	struct workqueue_struct	*flr_wq;
@@ -374,6 +376,22 @@ struct otx2_nic {
 
 	struct otx2_flow_config	*flow_cfg;
 	struct otx2_tc_info	tc_info;
+
+#define OTX2_PRIV_FLAG_PAM4			BIT(0)
+#define OTX2_PRIV_FLAG_EDSA_HDR			BIT(1)
+#define OTX2_PRIV_FLAG_HIGIG2_HDR		BIT(2)
+#define OTX2_IS_EDSA_ENABLED(flags)		((flags) &              \
+						 OTX2_PRIV_FLAG_EDSA_HDR)
+#define OTX2_IS_HIGIG2_ENABLED(flags)		((flags) &              \
+						 OTX2_PRIV_FLAG_HIGIG2_HDR)
+	u32		        ethtool_flags;
+
+	/* extended DSA and EDSA  header lengths are 8/16 bytes
+	 * so take max length 16 bytes here
+	 */
+#define OTX2_EDSA_HDR_LEN			16
+#define OTX2_HIGIG2_HDR_LEN			16
+	u32			addl_mtu;
 };
 
 static inline bool is_otx2_lbkvf(struct pci_dev *pdev)
@@ -814,6 +832,7 @@ int otx2_open(struct net_device *netdev);
 int otx2_stop(struct net_device *netdev);
 int otx2_set_real_num_queues(struct net_device *netdev,
 			     int tx_queues, int rx_queues);
+int otx2_set_npc_parse_mode(struct otx2_nic *pfvf);
 /* MCAM filter related APIs */
 int otx2_mcam_flow_init(struct otx2_nic *pf);
 int otx2_alloc_mcam_entries(struct otx2_nic *pfvf);
