96ec712f0d2048ae27fc4768c8b5fd42886a9141
Nithin Dabilpuram <ndabilpuram@marvell.com>
2020-01-18 17:59:50 +0530

    octeontx2-af: enable tx shaping feature for 96xx C0
    commit a477553bb04b25fbd61a3ddf91544992d3e7ff08 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Enable shaping for 96xx C0. Along with enabling shaping
    feature, add support to poll or wait everytime shaping
    configuration is toggled from enabled to disable and
    vice versa.
    
    This patch also fixes level used in resetting link config
    register by using NIX_AF_PSE_CHANNEL_LEVEL config.
    
    Change-Id: I6ffee2f3637dd67cf6066e98674f5b7c2619fb67
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/24953
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -70,10 +70,11 @@ static void rvu_setup_hw_capabilities(struct rvu *rvu)
 	hw->cap.nix_shaping = true;
 	hw->cap.nix_tx_link_bp = true;
 	hw->cap.nix_rx_multicast = true;
+	hw->cap.nix_shaper_toggle_wait = false;
 	hw->rvu = rvu;
 
 	if (is_rvu_96xx_B0(rvu) || is_rvu_95xx_A0(rvu) ||
-	    is_rvu_post_96xx_C0(rvu) || is_rvu_95xx_B0(rvu)) {
+	    is_rvu_95xx_B0(rvu)) {
 		hw->cap.nix_fixed_txschq_mapping = true;
 		hw->cap.nix_txsch_per_cgx_lmac = 4;
 		hw->cap.nix_txsch_per_lbk_lmac = 132;
@@ -86,6 +87,9 @@ static void rvu_setup_hw_capabilities(struct rvu *rvu)
 
 	if (!is_rvu_otx2(rvu))
 		hw->cap.per_pf_mbox_regs = true;
+
+	if (is_rvu_post_96xx_C0(rvu))
+		hw->cap.nix_shaper_toggle_wait = true;
 }
 
 /* Poll a RVU block's register 'offset', for a 'zero'
