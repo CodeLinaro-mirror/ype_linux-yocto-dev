12bbd8813557f98879dcec83fba48eb37d39d73f
Stanislaw Kardach <skardach@marvell.com>
2020-01-24 16:16:38 +0100

    octeontx2-af: fix memory leaks on probe failure
    commit 4e3843a1a6f2e39238d4b8b8f54516a8e0845739 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Ensure all allocated memory is freed when given HW block initialization
    fails.
    
    Change-Id: I058301b8ed7b1cd827ce0f045e1557163c2073cc
    Signed-off-by: Stanislaw Kardach <skardach@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/22296
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    [RQ: Drop the parts which have been already applied.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -1049,15 +1049,15 @@ cpt:
 
 	err = rvu_sso_init(rvu);
 	if (err)
-		goto cgx_err;
+		goto sso_err;
 
 	err = rvu_tim_init(rvu);
 	if (err)
-		goto cgx_err;
+		goto sso_err;
 
 	err = rvu_sdp_init(rvu);
 	if (err)
-		goto cgx_err;
+		goto sso_err;
 
 	/* Assign MACs for CGX mapped functions */
 	rvu_setup_pfvf_macaddress(rvu);
@@ -1080,6 +1080,8 @@ nix_err:
 	rvu_nix_freemem(rvu);
 npa_err:
 	rvu_npa_freemem(rvu);
+sso_err:
+	rvu_sso_freemem(rvu);
 cgx_err:
 	rvu_cgx_exit(rvu);
 npc_err:
