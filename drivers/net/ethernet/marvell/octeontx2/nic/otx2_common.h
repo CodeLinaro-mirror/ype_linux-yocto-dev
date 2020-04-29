ec70ccaa5dc5e2738d1af6a742a2555ffdc6501a
Hariprasad Kelam <hkelam@marvell.com>
2020-04-29 17:28:30 +0530

    octeontx2-pf: Add ntuple filter support for FDSA
    commit 0f462c799b44f705662c01a57ee6c6847a86cb67 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Marvell switches support FDSA (Forward DSA). FDSA has
    4 bytes length and it contains Src port and vlan field.
    KPU profile is updated to parse  FDSA packet and extract Src port. The
    extracted Src port is placed in vlan field of KEX key.
    
    This patch adds ntuple filter support to FDSA tag.
    So that user can send traffic to either PF or VF based on
    Src port or Vlan id. All rules installed for FDSA has default
    action as RSS. Upon enabling FDSA , vf vlan rules will be disabled.
    
    To enable fdsa tag
    ethtool --set-priv-flags eth0 fdsa on
    
    To send traffic with Srcport 30 to PF
    ethtool -U eth0 flow-type ether  user-def 0x1e
    
    To send traffic with vlan id 30 to PF
    ethtool -U eth0 flow-type ether  dst xx vlan 30 m 0xf000
    
    To send traffic with Srcport 20 to vf 0
    ethtool -U eth0 flow-type ether vf 0 user-def 0x14
    
    Change-Id: I547296bb497e73b5b048297b8459543cd5d3bfb5
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/27668
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    [RQ: Put struct vfvlan before struct otx2_flow_config.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -206,6 +206,12 @@ struct otx2_hw {
 	u8			lbk_links;  /* No. of LBK links present in HW */
 };
 
+struct vfvlan {
+	u16 vlan;
+	u16 proto;
+	u8 qos;
+};
+
 struct otx2_vf_config {
 	struct otx2_nic *pf;
 	struct delayed_work link_event_work;
@@ -214,6 +220,7 @@ struct otx2_vf_config {
 	u8 mac[ETH_ALEN];
 	u16 vlan;
 	int tx_vtag_idx;
+	struct vfvlan rule;
 };
 
 struct flr_work {
@@ -237,6 +244,7 @@ struct otx2_ptp {
 };
 
 #define OTX2_HW_TIMESTAMP_LEN	8
+#define OTX2_FDSA_HDR_LEN			4
 
 struct otx2_mac_table {
 	u8 addr[ETH_ALEN];
@@ -321,7 +329,9 @@ struct otx2_nic {
 #define OTX2_PRIV_FLAG_PAM4			BIT(0)
 #define OTX2_PRIV_FLAG_EDSA_HDR			BIT(1)
 #define OTX2_PRIV_FLAG_HIGIG2_HDR		BIT(2)
-#define OTX2_PRIV_FLAG_DEF_MODE			BIT(3)
+#define OTX2_PRIV_FLAG_FDSA_HDR			BIT(3)
+#define OTX2_INTF_MOD_MASK			GENMASK(3, 1)
+#define OTX2_PRIV_FLAG_DEF_MODE			BIT(4)
 #define OTX2_IS_EDSA_ENABLED(flags)		((flags) &              \
 						 OTX2_PRIV_FLAG_EDSA_HDR)
 #define OTX2_IS_HIGIG2_ENABLED(flags)		((flags) &              \
@@ -740,6 +750,9 @@ int otx2_get_all_flows(struct otx2_nic *pfvf,
 int otx2_add_flow(struct otx2_nic *pfvf,
 		  struct ethtool_rx_flow_spec *fsp);
 int otx2_remove_flow(struct otx2_nic *pfvf, u32 location);
+int otx2_prepare_flow_request(struct ethtool_rx_flow_spec *fsp,
+			      struct npc_install_flow_req *req,
+			      struct otx2_nic *pfvf);
 int otx2smqvf_probe(struct otx2_nic *vf);
 int otx2smqvf_remove(struct otx2_nic *vf);
 int otx2_del_macfilter(struct net_device *netdev, const u8 *mac);
@@ -747,5 +760,7 @@ int otx2_add_macfilter(struct net_device *netdev, const u8 *mac);
 int otx2_enable_rxvlan(struct otx2_nic *pf, bool enable);
 int otx2_enable_vf_vlan(struct otx2_nic *pf);
 int otx2_install_rxvlan_offload_flow(struct otx2_nic *pfvf);
+int otx2_do_set_vf_vlan(struct otx2_nic *pf, int vf, u16 vlan, u8 qos,
+			u16 proto);
 
 #endif /* OTX2_COMMON_H */
