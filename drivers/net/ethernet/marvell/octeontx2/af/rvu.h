a529c0087301f06df9e5cb3c6d8292ee2d25125f
Smadar Fuks <smadarf@marvell.com>
2020-05-26 14:45:51 -0400

    octeontx2-af: Introducing REE block for 98xx
    commit 3a2d554772754d1a0595d355dc9ede9ce6d09f61 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    CN98XX Regular Expression Engine (REE) includes a two-part regular
    expression search engine consisting of filter engine and graph-walk
    engines. Input data (job) is searched against the regular expression
    rule set by first traversing the filter engine and if matched passing
    full Thompson NFA graph walk.
    These 2 REE blocks (REE0 and REE1) are introduced in 98xx.
    REE is provisioned using RVU.
    REE has up to 36 local functions (queues)
    
    A new file rvu_ree.c is added to support the new REE block with the
    following functions:
    - Initialization
    - Free memory
    - Interrupt registration and handling
    
    The following changes were done in rvu.c for ree:
    - Added calls to ree functions init, free memory, register/unregister
      interrupts and teardown
    - Added member struct ree_rsrc in struct rvu_pfvf to hold ree data
    - Added member struct rvu *rvu in struct rvu_block so that interrupt
      handler will know the block address
    
    Change-Id: I33184efaed9bab2954c8604d462b8945dda892d4
    Signed-off-by: Smadar Fuks <smadarf@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/29300
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -114,6 +114,7 @@ struct rvu_block {
 	u64  msixcfg_reg;
 	u64  lfreset_reg;
 	unsigned char name[NAME_SIZE];
+	struct rvu *rvu;
 };
 
 struct nix_mcast {
@@ -203,6 +204,17 @@ struct sso_rsrc {
 	u64	taq_rsvd;
 	u64	taq_max;
 	struct rsrc_bmap pfvf_ident;
+};
+
+struct ree_rsrc {
+	struct qmem	*graph_ctx;	/* Graph base address - used by HW */
+	struct qmem	*prefix_ctx;	/* Prefix blocks - used by HW */
+	void		**ruledb;	/* ROF file from application */
+	u8		*ruledbi;	/* Incremental checksum instructions */
+	u32		aq_head;	/* AF AQ head address */
+	u32		ruledb_len;	/* Length of ruledb */
+	u32		ruledbi_len;	/* Length of ruledbi */
+	u8		ruledb_blocks;	/* Number of blocks pointed by ruledb */
 };
 
 /* Structure for per RVU func info ie PF/VF */
@@ -374,6 +386,7 @@ struct rvu_hwinfo {
 	struct npc_pkind pkind;
 	struct npc_mcam  mcam;
 	struct sso_rsrc  sso;
+	struct ree_rsrc *ree;
 };
 
 struct mbox_wq_info {
@@ -783,6 +796,12 @@ bool is_mcam_entry_enabled(struct rvu *rvu, struct npc_mcam *mcam, int blkaddr,
 /* SDP APIs */
 int rvu_sdp_init(struct rvu *rvu);
 bool is_sdp_pf(u16 pcifunc);
+
+/* REE APIs */
+int rvu_ree_init(struct rvu *rvu);
+void rvu_ree_freemem(struct rvu *rvu);
+int rvu_ree_register_interrupts(struct rvu *rvu);
+void rvu_ree_unregister_interrupts(struct rvu *rvu);
 
 /* CPT APIs */
 int rvu_cpt_register_interrupts(struct rvu *rvu);
