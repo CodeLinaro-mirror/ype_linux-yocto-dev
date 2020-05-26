b69ccd15a8d7a0369185e56f8bf1a15cde72cea2
Smadar Fuks <smadarf@marvell.com>
2020-05-26 14:45:51 -0400

    octeontx2-af: Introducing REE block for 98xx
    commit a4d454585655319d82d1e97283a91d356ba1d72d from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    CN98XX Regular Expression Engine (REE) includes a two-part regular
    expression search engine consisting of filter engine and graph-walk
    engines. Input data (job) is searched against the regular expression
    rule set by first traversing the filter engine and if matched passing
    full Thompson NFA graph walk.
    These 2 REE blocks (REE0 and REE1) are introduced in 98xx.
    REE is provisioned using RVU.
    REE has up to 36 local functions (queues)
    
    A new file rvu_ree.c is added to support the new REE block with the
    following functions:
    - Initialization
    - Free memory
    - Interrupt registration and handling
    
    The following changes were done in rvu.c for ree:
    - Added calls to ree functions init, free memory, register/unregister
      interrupts and teardown
    - Added member struct ree_rsrc in struct rvu_pfvf to hold ree data
    - Added member struct rvu *rvu in struct rvu_block so that interrupt
      handler will know the block address
    
    Change-Id: I33184efaed9bab2954c8604d462b8945dda892d4
    Signed-off-by: Smadar Fuks <smadarf@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/29293
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_struct.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_struct.h
@@ -86,6 +86,15 @@ enum npa_af_int_vec_e {
 	NPA_AF_INT_VEC_CNT	= 0x5,
 };
 
+/* REE Admin function Interrupt Vector Enumeration */
+enum ree_af_int_vec_e {
+	REE_AF_INT_VEC_RAS	= 0x0,
+	REE_AF_INT_VEC_RVU	= 0x1,
+	REE_AF_INT_VEC_QUE_DONE	= 0x2,
+	REE_AF_INT_VEC_AQ	= 0x3,
+	REE_AF_INT_VEC_CNT	= 0x4,
+};
+
 /* NIX Admin function Interrupt Vector Enumeration */
 enum nix_af_int_vec_e {
 	NIX_AF_INT_VEC_RVU	= 0x0,
@@ -741,4 +750,25 @@ enum nix_tx_vtag_op {
 #define VTAG_STRIP	BIT_ULL(4)
 #define VTAG_CAPTURE	BIT_ULL(5)
 
+/* REE admin queue instruction structure */
+struct ree_af_aq_inst_s {
+	u64 rof_ptr_addr;
+#if defined(__BIG_ENDIAN_BITFIELD)
+	u64 reserved_111_127	: 17;
+	u64 length		: 15;
+	u64 reserved_68_95	: 28;
+	u64 doneint		:  1;
+	u64 reserved_66_66	:  1;
+	u64 nc			:  1;
+	u64 reserved_64_64	:  1;
+#else
+	u64 reserved_64_64	:  1;
+	u64 nc			:  1;
+	u64 reserved_66_66	:  1;
+	u64 doneint		:  1;
+	u64 reserved_68_95	: 28;
+	u64 length		: 15;
+	u64 reserved_111_127	: 17;
+#endif
+};
 #endif /* RVU_STRUCT_H */
