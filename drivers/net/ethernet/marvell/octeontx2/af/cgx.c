eb437c1c7b2a008a623dd0ea77331cf7550edc98
Subbaraya Sundeep <sbhatta@marvell.com>
2020-10-23 18:26:31 +0530

    octeontx2-af: Fix static code analyzer reported issues
    commit bf9292729868b61c2831461542a5b89f691ea099 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch fixes below issues reported by static code
    analyzer:
            Possible null pointer dereference
            Different sizes of operands in bitops
            Returning uninitialized values
    
    Change-Id: Id4093e850284761d5ad888f96c5ba8ec1e2e68a6
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/30826
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.c
@@ -254,6 +254,9 @@ int cgx_lmac_addr_add(u8 cgx_id, u8 lmac_id, u8 *mac_addr)
 	int index, idx;
 	u64 cfg = 0;
 
+	if (!lmac)
+		return -ENODEV;
+
 	mac_ops = cgx_dev->mac_ops;
 
 	/* Get available index where entry is to be installed */
@@ -286,6 +289,9 @@ int cgx_lmac_addr_reset(u8 cgx_id, u8 lmac_id)
 	u8 index = 0;
 	u64 cfg;
 
+	if (!lmac)
+		return -ENODEV;
+
 	mac_ops = cgx_dev->mac_ops;
 
 	/* Restore index 0 to its default init value as done during
@@ -311,6 +317,9 @@ int cgx_lmac_addr_del(u8 cgx_id, u8 lmac_id, u8 index)
 	struct mac_ops *mac_ops;
 	struct cgx *cgx_dev = cgx_get_pdata(cgx_id);
 	struct lmac *lmac = lmac_pdata(lmac_id, cgx_dev);
+
+	if (!lmac)
+		return -ENODEV;
 
 	mac_ops = cgx_dev->mac_ops;
 	/* Validate the index */
