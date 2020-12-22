038de105a108ea9627f94355cc7dab1360311e64
Naveen Mamindlapalli <naveenm@marvell.com>
2020-12-22 12:35:57 +0530

    octeontx2-pf: Backport upstream kernel changes
    commit 050a63947e609573102a99e405033f7f90d32849 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Backported PF/VF changes from upstream kernel.
    -> Avoid modifying netdev->features dynamically by the driver.
       https://www.kernel.org/doc/html/latest/networking/netdev-features.html
    -> The MCAM entry allocation is done statically during initialization
       time. The netdev->features is initialized based on the entries
       allocated at the low level.
    -> Removed ndo_features_check callback.
    -> Used eth_broadcast_addr, eth_zero_addr where ever applicable.
    -> Use -EOPNOTSUPP instead of -ENOTSUPP.
    
    Signed-off-by: Naveen Mamindlapalli <naveenm@marvell.com>
    Change-Id: I6892d1abce6434b76aa32e26a49beb8630ea6418
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/42675
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    [RQ: Drop the parts which have been already applied.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -2341,13 +2341,6 @@ static int otx2_get_vf_config(struct net_device *netdev, int vf,
 	return 0;
 }
 
-static netdev_features_t
-otx2_features_check(struct sk_buff *skb, struct net_device *dev,
-		    netdev_features_t features)
-{
-	return features;
-}
-
 static int otx2_xdp_xmit_tx(struct otx2_nic *pf, struct xdp_frame *xdpf,
 			    int qidx)
 {
@@ -2465,7 +2458,6 @@ static const struct net_device_ops otx2_netdev_ops = {
 	.ndo_set_vf_mac		= otx2_set_vf_mac,
 	.ndo_set_vf_vlan	= otx2_set_vf_vlan,
 	.ndo_get_vf_config	= otx2_get_vf_config,
-	.ndo_features_check     = otx2_features_check,
 	.ndo_bpf		= otx2_xdp,
 	.ndo_xdp_xmit           = otx2_xdp_xmit,
 	.ndo_setup_tc		= otx2_setup_tc,
@@ -2668,7 +2660,7 @@ static int otx2_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 			       NETIF_F_GSO_UDP_L4);
 	netdev->features |= netdev->hw_features;
 
-	netdev->hw_features |= NETIF_F_LOOPBACK | NETIF_F_RXALL | NETIF_F_NTUPLE;
+	netdev->hw_features |= NETIF_F_LOOPBACK | NETIF_F_RXALL;
 
 	err = otx2_mcam_flow_init(pf);
 	if (err)
