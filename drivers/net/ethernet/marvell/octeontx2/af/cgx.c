98cffe58e97b7616d40490665274ac0c3773f074
Subbaraya Sundeep <sbhatta@marvell.com>
2020-12-30 19:07:17 +0530

    octeontx2-af: Disable packet TX and RX
    commit ba9834332a3750783e1681d272f79128242e0b70 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The NPC RX miss stats counter increases continuously
    though no interface is brought up by user. This
    is because firmware enables packet I/O when
    LINK_BRING_UP command is sent to it by kernel.
    Hence disable packet I/O in kernel after link
    up sequence. There is still a chance of some
    packets reaching NPC because of the time gap
    between firmware enabling packet I/O and AF
    driver disabling it. This patch also disables
    packet I/O during driver init for a sane
    state. When interfaces are brought up then
    packet I/O is enabled eventually.
    
    Change-Id: I816c3e4631ba1bc62541ab94547a374cf936bf52
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/43371
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.c
@@ -1417,6 +1417,10 @@ static void cgx_lmac_linkup_work(struct work_struct *work)
 		if (err)
 			dev_info(dev, "cgx port %d:%d Link up command failed\n",
 				 cgx->cgx_id, i);
+		/* Disable packet I/O in CGX in case firmware enabled it
+		 * after Link up sequence.
+		 */
+		cgx_lmac_rx_tx_enable(cgx, i, false);
 	}
 }
 
@@ -1585,6 +1589,8 @@ static int cgx_lmac_init(struct cgx *cgx)
 		/* Add reference */
 		cgx->lmac_idmap[lmac->lmac_id] = lmac;
 		cgx->mac_ops->mac_pause_frm_config(cgx, lmac->lmac_id, true);
+		/* Disable packet I/O for a sane state */
+		cgx_lmac_rx_tx_enable(cgx, lmac->lmac_id, false);
 		set_bit(lmac->lmac_id, &cgx->lmac_bmap);
 	}
 
