e08a17325e7e7d3ab85d5a5cc161f3104aad70b8
Nalla, Pradeep <pnalla@marvell.com>
2021-01-25 16:18:25 +0530

    octeontx2-af: Mbox to exchange SDP channel info
    commit 8542273b511c8ef3b9f246a170546b41447c8240 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    New mbox is added to share SDP channel information that SDP PF posses by
    parsing DTS node, with the AF driver. AF driver uses this information to
    install promisc entry while processing LF alloc request from a SDP nix
    interface. LF alloc response from AF is modified by PF, according to
    prior exchanged ring information with host, before forwarding to
    corresponding VF.
    
    Also Backpressure is enabled for the channels assigned to a SDP NIX
    interface in LF alloc request.
    
    Change-Id: I5acedc7c2eba02e3c5320bd5363e83b678a5229c
    Signed-off-by: Nalla, Pradeep <pnalla@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/44344
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -284,12 +284,11 @@ struct rvu_pfvf {
 	u8	nix_blkaddr; /* BLKADDR_NIX0/1 assigned to this PF */
 	u8	nix_rx_intf; /* NIX0_RX/NIX1_RX interface to NPC */
 	u8	nix_tx_intf; /* NIX0_TX/NIX1_TX interface to NPC */
-	bool	is_sdp0; /* is this PF mapped to SDP0 */
-	bool	is_sdp1; /* is this PF mapped to SDP1 */
 	u64     lmt_base_addr; /* Preseving the pcifunc's lmtst base addr*/
 	u64     lmt_map_ent_w1; /*Preseving the word1 of lmtst map table entry*/
 	unsigned long flags;
 	int     intf_mode;
+	struct  sdp_node_info *sdp_info;
 };
 
 enum rvu_pfvf_flags {
@@ -705,13 +704,17 @@ int rvu_aq_alloc(struct rvu *rvu, struct admin_queue **ad_queue,
 		 int qsize, int inst_size, int res_size);
 void rvu_aq_free(struct rvu *rvu, struct admin_queue *aq);
 
+/* SDP APIs */
+int rvu_sdp_init(struct rvu *rvu);
+bool is_sdp_pfvf(u16 pcifunc);
+bool is_sdp_pf(u16 pcifunc);
+bool is_sdp_vf(u16 pcifunc);
+
 /* CGX APIs */
 static inline bool is_pf_cgxmapped(struct rvu *rvu, u8 pf)
 {
-	struct rvu_pfvf *pfvf = &rvu->pf[pf];
-
 	return (pf >= PF_CGXMAP_BASE && pf <= rvu->cgx_mapped_pfs) &&
-		(!pfvf->is_sdp0 && !pfvf->is_sdp1);
+		!is_sdp_pf(pf << RVU_PFVF_PF_SHIFT);
 }
 
 static inline void rvu_get_cgx_lmac_id(u8 map, u8 *cgx_id, u8 *lmac_id)
@@ -827,12 +830,6 @@ int npc_get_nixlf_mcam_index(struct npc_mcam *mcam, u16 pcifunc, int nixlf,
 			     int type);
 bool is_mcam_entry_enabled(struct rvu *rvu, struct npc_mcam *mcam, int blkaddr,
 			   int index);
-
-/* SDP APIs */
-int rvu_sdp_init(struct rvu *rvu);
-bool is_sdp_pfvf(u16 pcifunc);
-bool is_sdp_pf(u16 pcifunc);
-bool is_sdp_vf(u16 pcifunc);
 
 /* REE APIs */
 int rvu_ree_init(struct rvu *rvu);
