f064796ff26073da23f92fb84e73a577b8634ef7
Geetha sowjanya <gakula@marvell.com>
2021-02-15 11:45:02 +0530

    octeontx2-af: Add F95XXO RVU PCI revision ID
    commit fd11362744e280c2300225cf72d2049e54b039f5 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch adds F95O PCI revision id to the platform check
    and PTP subsystem id to the PCI device table.
    
    Change-Id: I7d18441276cc487485fd40d9e224f9e91f538e36
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/45975
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -73,8 +73,7 @@ static void rvu_setup_hw_capabilities(struct rvu *rvu)
 	hw->cap.nix_shaper_toggle_wait = false;
 	hw->rvu = rvu;
 
-	if (is_rvu_96xx_B0(rvu) || is_rvu_95xx_A0(rvu) ||
-	    is_rvu_95xx_B0(rvu)) {
+	if (is_rvu_pre_96xx_C0(rvu)) {
 		hw->cap.nix_fixed_txschq_mapping = true;
 		hw->cap.nix_txsch_per_cgx_lmac = 4;
 		hw->cap.nix_txsch_per_lbk_lmac = 132;
@@ -88,7 +87,7 @@ static void rvu_setup_hw_capabilities(struct rvu *rvu)
 	if (!is_rvu_otx2(rvu))
 		hw->cap.per_pf_mbox_regs = true;
 
-	if (is_rvu_post_96xx_C0(rvu))
+	if (!is_rvu_pre_96xx_C0(rvu))
 		hw->cap.nix_shaper_toggle_wait = true;
 }
 
