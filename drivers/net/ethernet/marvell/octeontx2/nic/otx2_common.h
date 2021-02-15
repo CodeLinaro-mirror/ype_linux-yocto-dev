bc9db2abd90f052848568776e151dee552e5cf7d
Geetha sowjanya <gakula@marvell.com>
2021-02-15 11:16:29 +0530

    octeontx2-af: Add F95XXO RVU PCI revision ID
    commit 93c3109abfa806f2fb5d4772677c684b9483f508 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch adds F95O PCI revision id to the platform check
    and PTP subsystem id to the PCI device table.
    
    Change-Id: Ia5653cc9c46d51c17fe33c30a6ba08abb872fe6e
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/45974
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -413,11 +413,6 @@ static inline bool is_otx2_lbkvf(struct pci_dev *pdev)
 	return pdev->device == PCI_DEVID_OCTEONTX2_RVU_AFVF;
 }
 
-static inline bool is_dev_post_96xx_C0(struct pci_dev *pdev)
-{
-	return (pdev->revision == 0x08) || (pdev->revision == 0x30);
-}
-
 static inline bool is_96xx_A0(struct pci_dev *pdev)
 {
 	return (pdev->revision == 0x00);
@@ -443,6 +438,7 @@ static inline bool is_96xx_B0(struct pci_dev *pdev)
 #define PCI_REVISION_ID_LOKI		0x20
 #define PCI_REVISION_ID_98XX		0x30
 #define PCI_REVISION_ID_95XXMM		0x40
+#define PCI_REVISION_ID_95XXO		0xE0
 
 static inline bool is_dev_otx2(struct pci_dev *pdev)
 {
@@ -450,7 +446,7 @@ static inline bool is_dev_otx2(struct pci_dev *pdev)
 
 	return (midr == PCI_REVISION_ID_96XX || midr == PCI_REVISION_ID_95XX ||
 		midr == PCI_REVISION_ID_LOKI || midr == PCI_REVISION_ID_98XX ||
-		midr == PCI_REVISION_ID_95XXMM);
+		midr == PCI_REVISION_ID_95XXMM || midr == PCI_REVISION_ID_95XXO);
 }
 
 static inline void otx2_setup_dev_hw_settings(struct otx2_nic *pfvf)
