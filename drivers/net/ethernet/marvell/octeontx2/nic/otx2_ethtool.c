af4d5358d85822c8556e2557bb4815066821d09a
Hariprasad Kelam <hkelam@marvell.com>
2021-03-05 14:47:40 +0530

    octeontx2-pf: clean up changes for FEC configuration
    commit 8a2c4521ecd3ef2833bed80ad54ddcc2bcf9b64d from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch addresses indentation issues and code placement issues.
    
    Change-Id: Iee214a4cf9a531931a73952db2968276078c13a5
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/47344
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    [RQ: Drop the parts which have been already applied.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
@@ -22,8 +22,6 @@
 #define DRV_NAME	"octeontx2-nicpf"
 #define DRV_VF_NAME	"octeontx2-nicvf"
 
-static struct cgx_fw_data *otx2_get_fwdata(struct otx2_nic *pfvf);
-
 static const char otx2_priv_flags_strings[][ETH_GSTRING_LEN] = {
 	"pam4",
 	"edsa",
