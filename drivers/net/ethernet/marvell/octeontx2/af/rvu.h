0d109563e92f7cc313b6817f6882b42ba767710d
Sunil Goutham <sgoutham@marvell.com>
2021-03-12 17:29:10 +0530

    octeontx2-af: cn10k: Bandwidth profiles config support
    commit dae6bd23dd0123bb97ec3cd9f976b0ef006f6497 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    CN10K silicons supports hierarchial ingress packet ratelimiting.
    There are 3 levels of profilers supported leaf, mid and top.
    Ratelimiting is done after packet forwarding decision is taken
    and a NIXLF's RQ is identified to DMA the packet. RQ's context
    points to a leaf bandwidth profile which can be configured
    to achieve desired ratelimit.
    
    This patch adds logic for management of these bandwidth profiles
    ie profile alloc, free, context update etc.
    
    Change-Id: Id538740022146b26c77dc80f740b3e0c3dd865c8
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/48869
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    [RQ: Fix compile warning.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -336,6 +336,11 @@ struct nix_txvlan {
 	struct mutex rsrc_lock; /* Serialize resource alloc/free */
 };
 
+struct nix_ipolicer {
+	struct rsrc_bmap band_prof;
+	u16 *pfvf_map;
+};
+
 struct nix_hw {
 	int blkaddr;
 	struct rvu *rvu;
@@ -346,6 +351,7 @@ struct nix_hw {
 	struct nix_lso lso;
 	void   *tx_stall;
 	struct nix_txvlan txvlan;
+	struct nix_ipolicer *ipolicer;
 	u64    *tx_credits;
 };
 
@@ -365,6 +371,7 @@ struct hw_cap {
 	bool	nix_rx_multicast;	 /* Rx packet replication support */
 	bool	per_pf_mbox_regs; /* PF mbox specified in per PF registers ? */
 	bool	programmable_chans; /* Channels programmable ? */
+	bool	ipolicer;
 };
 
 struct rvu_hwinfo {
