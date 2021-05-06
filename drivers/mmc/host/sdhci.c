4ba336a21d24835d18da5859ddde140a72c5f090
Jayanthi Annadurai <jannadurai@marvell.com>
2021-05-05 23:25:07 -0700

    drivers: sdhci: CN10K ASIM workaround
    commit 099bf79cbceefceceab192f2d04196448604cad3 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Cadence SD6 controller supports 8/16/32 bit writes. But, the read
    is only 32bits. Workaround asim limitations by accessing 32bits for
    both read and write.
    
    Change-Id: If7bf5504832543476725dea711b6aa7de2b52c0d
    Signed-off-by: Jayanthi Annadurai <jannadurai@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/51814
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -42,10 +42,9 @@
 #define SDHCI_DUMP(f, x...) \
 	pr_err("%s: " DRIVER_NAME ": " f, mmc_hostname(host->mmc), ## x)
 
-#define CN10K_ASIM_WORKAROUND	1
 #define MAX_TUNING_LOOP 40
 
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 static int trfr_mode;
 #endif
 
@@ -1415,7 +1414,7 @@ static inline void sdhci_auto_cmd_select(struct sdhci_host *host,
 		*mode |= SDHCI_TRNS_AUTO_CMD23;
 }
 
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 static u16 sdhci_set_transfer_mode(struct sdhci_host *host,
 	struct mmc_command *cmd)
 #else
@@ -1431,7 +1430,7 @@ static void sdhci_set_transfer_mode(struct sdhci_host *host,
 			SDHCI_QUIRK2_CLEAR_TRANSFERMODE_REG_BEFORE_CMD) {
 			/* must not clear SDHCI_TRANSFER_MODE when tuning */
 			if (cmd->opcode != MMC_SEND_TUNING_BLOCK_HS200)
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 				mode = 0;
 #else
 				sdhci_writew(host, 0x0, SDHCI_TRANSFER_MODE);
@@ -1439,7 +1438,7 @@ static void sdhci_set_transfer_mode(struct sdhci_host *host,
 		} else {
 		/* clear Auto CMD settings for no data CMDs */
 			mode = sdhci_readw(host, SDHCI_TRANSFER_MODE);
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 			mode = 0;
 #else
 			mode = (mode & ~(SDHCI_TRNS_AUTO_CMD12 |
@@ -1448,7 +1447,7 @@ static void sdhci_set_transfer_mode(struct sdhci_host *host,
 				SDHCI_TRNS_AUTO_CMD23), SDHCI_TRANSFER_MODE);
 #endif
 		}
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 		return mode;
 #else
 		return;
@@ -1472,7 +1471,7 @@ static void sdhci_set_transfer_mode(struct sdhci_host *host,
 	if (host->flags & SDHCI_REQ_USE_DMA)
 		mode |= SDHCI_TRNS_DMA;
 
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 	return mode;
 #else
 	sdhci_writew(host, mode, SDHCI_TRANSFER_MODE);
@@ -1622,7 +1621,7 @@ static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	int flags;
 	u32 mask;
 	unsigned long timeout;
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 	u32 mode, cmdreg32;
 #endif
 
@@ -1664,7 +1663,7 @@ static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 
 	sdhci_writel(host, cmd->arg, SDHCI_ARGUMENT);
 
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 	mode = sdhci_set_transfer_mode(host, cmd);
 #else
 	sdhci_set_transfer_mode(host, cmd);
@@ -1712,7 +1711,7 @@ static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	if (host->use_external_dma)
 		sdhci_external_dma_pre_transfer(host, cmd);
 
-#ifdef CN10K_ASIM_WORKAROUND
+#ifdef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 	if (trfr_mode) {
 		cmdreg32 = SDHCI_TRNS_READ | mode | (SDHCI_MAKE_CMD(cmd->opcode, flags) << 16);
 		trfr_mode = 0;
@@ -2772,7 +2771,7 @@ void sdhci_send_tuning(struct sdhci_host *host, u32 opcode)
 	 * This also takes care of setting DMA Enable and Multi Block
 	 * Select in the same register to 0.
 	 */
-#ifndef CN10K_ASIM_WORKAROUND
+#ifndef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 	sdhci_writew(host, SDHCI_TRNS_READ, SDHCI_TRANSFER_MODE);
 
 #else
@@ -2803,7 +2802,7 @@ static int __sdhci_execute_tuning(struct sdhci_host *host, u32 opcode)
 {
 	int i;
 
-#ifndef CN10K_ASIM_WORKAROUND
+#ifndef CONFIG_MMC_SDHCI_CADENCE_WORKAROUND
 	return 0;
 #endif
 	/*
