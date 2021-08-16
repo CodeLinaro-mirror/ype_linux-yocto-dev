WindRiver-Labs/linux-yocto-5.10

drivers:cpt - add support for CPT1 in VF driver
commit 2816f08d991a6c89c2a25f8fd7629f10a87b035d from
git@git.assembla.com:cavium/WindRiver.linux.git

Adds support for CPT1 in VF driver. User may specify
the cpt block number(0 or 1, default 0) to which
CPT instructions may submit, through module
parameter to the VF

Change-Id: I95c134e1881f80f9c4a5fa8d4efcaed35a0916a3
Signed-off-by: Srujana Challa <schalla@marvell.com>
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/31174
Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
Reviewed-by: Mahipal Challa <mchalla@marvell.com>
Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

drivers/crypto/marvell/octeontx2/otx2_cptvf_mbox.c
@@ -110,6 +110,10 @@ void otx2_cptvf_pfvf_mbox_handler(struct work_struct *work)
 			for (j = 0; j < rsp_msix->cptlfs; j++)
 				cptvf->lfs.lf[j].msix_offset =
 						rsp_msix->cptlf_msixoff[j];
+
+			for (j = 0; j < rsp_msix->cpt1_lfs; j++)
+				cptvf->lfs.lf[j].msix_offset =
+						rsp_msix->cpt1_lf_msixoff[j];
 			break;
 
 		case MBOX_MSG_CPT_RD_WR_REGISTER:

CodeLinaro-mirror/ype_linux-yocto-dev

drivers/crypto/marvell/octeontx2/otx2_cptvf_mbox.c
@@ -64,6 +64,8 @@ static void process_pfvf_mbox_mbox_msg(struct otx2_cptvf_dev *cptvf,
 		rsp_msix = (struct msix_offset_rsp *) msg;
 		for (i = 0; i < rsp_msix->cptlfs; i++)
 			lfs->lf[i].msix_offset = rsp_msix->cptlf_msixoff[i];
+		for (i = 0; i < rsp_msix->cpt1_lfs; i++)
+			cptvf->lfs.lf[i].msix_offset = rsp_msix->cpt1_lf_msixoff[i];
 		break;
 	case MBOX_MSG_CPT_RD_WR_REGISTER:
 		rsp_reg = (struct cpt_rd_wr_reg_msg *) msg;