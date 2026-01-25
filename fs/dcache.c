84bc38617779c96f6ebe2211a6e8f2e0e91f3b0a
Bruce Ashfield <bruce.ashfield@gmail.com>
2026-01-25 16:29:48 -0500

    aufs6: standalone
    Signed-off-by: Bruce Ashfield <bruce.ashfield@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -1411,6 +1411,7 @@ rename_retry:
 	seq = 1;
 	goto again;
 }
+EXPORT_SYMBOL_GPL(d_walk);
 
 struct check_mount {
 	struct vfsmount *mnt;
--- fs/namespace.c
+++ fs/namespace.c
@@ -959,6 +959,7 @@ int is_current_mnt_ns(struct vfsmount *mnt)
 {
 	return check_mnt(real_mount(mnt));
 }
+EXPORT_SYMBOL_GPL(is_current_mnt_ns);
 
 static inline bool check_anonymous_mnt(struct mount *mnt)
 {
@@ -2280,6 +2281,7 @@ const struct path *collect_paths(const struct path *path,
 		path_get(p);
 	return res;
 }
+EXPORT_SYMBOL_GPL(collect_paths);
 
 void drop_collected_paths(const struct path *paths, const struct path *prealloc)
 {
@@ -2288,6 +2290,7 @@ void drop_collected_paths(const struct path *paths, const struct path *prealloc)
 	if (paths != prealloc)
 		kfree(paths);
 }
+EXPORT_SYMBOL_GPL(drop_collected_paths);
 
 static struct mnt_namespace *alloc_mnt_ns(struct user_namespace *, bool);
 
